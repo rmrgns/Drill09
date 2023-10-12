2ec167fbd864fd6cb7ebbe76c895a8e8ce0f0668
Jung Geuk Hun <111038432+rmrgns@users.noreply.github.com>
2023-10-12 12:22:02 +0900

    Drill 09 제출(2017180039정극훈)
    코드 리포맷

--- boy.py
+++ boy.py
@@ -8,24 +8,31 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK
 def space_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
 
+
 def time_out(e):
     return e[0] == 'TIME_OUT'
     # time_out = lambda e : e[0] == 'TIME_OUT'
 
+
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
 
 def right_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
 
+
 def left_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
 
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
+
 def press_a(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
+
 
 class Sleep:
 
@@ -40,13 +47,13 @@ class Sleep:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-        #print('드르렁')
+        # print('드르렁')
 
     @staticmethod
     def draw(boy):
         if boy.action == 2:
             boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100,
-                                      -1* math.pi / 2, '', boy.x + 25, boy.y - 25, 100, 100)
+                                          -1 * math.pi / 2, '', boy.x + 25, boy.y - 25, 100, 100)
         else:
             boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100,
                                           math.pi / 2, '', boy.x - 25, boy.y - 25, 100, 100)
@@ -79,13 +86,14 @@ class Idle:
     def draw(boy):
         boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
 
+
 class Run:
 
     @staticmethod
     def enter(boy, e):
-        if right_down(e) or left_up(e):     # 오른쪽 으로 RUN
+        if right_down(e) or left_up(e):  # 오른쪽 으로 RUN
             boy.dir, boy.action = 1, 1
-        elif left_down(e) or right_up(e):   # 왼쪽 으로 RUN
+        elif left_down(e) or right_up(e):  # 왼쪽 으로 RUN
             boy.dir, boy.action = -1, 0
 
     @staticmethod
@@ -101,13 +109,14 @@ class Run:
     def draw(boy):
         boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
 
+
 class AutoRun:
     @staticmethod
     def enter(boy, e):
-        if press_a(e):      # a 키를 눌렀을 경우
-            if boy.action == 2:     # 왼쪽 으로 AutoRun
+        if press_a(e):  # a 키를 눌렀을 경우
+            if boy.action == 2:  # 왼쪽 으로 AutoRun
                 boy.dir, boy.action = -1, 0
-            elif boy.action == 3:   # 오른쪽 으로 AutoRun
+            elif boy.action == 3:  # 오른쪽 으로 AutoRun
                 boy.dir, boy.action = 1, 1
 
     @staticmethod
@@ -122,13 +131,13 @@ class AutoRun:
             boy.dir, boy.action = -1, 0
         elif boy.x < 0:
             boy.dir, boy.action = 1, 1
-        #print('AutoRun')
+        # print('AutoRun')
         if get_time() - boy.wait_time > 5:
             boy.state_machine.handle_event(('TIME_OUT', 0))
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y+40,200, 200)
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y + 40, 200, 200)
 
 
 class StateMachine:
@@ -136,7 +145,7 @@ class StateMachine:
         self.boy = boy
         self.cur_state = Idle
         self.table = {
-            Sleep: {space_down: Idle, right_down: Run, left_down: Run, right_up: Run, left_up:Run},
+            Sleep: {space_down: Idle, right_down: Run, left_down: Run, right_up: Run, left_up: Run},
             Idle: {press_a: AutoRun, time_out: Sleep, right_down: Run, left_down: Run, left_up: Run, right_up: Run},
             Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
             AutoRun: {time_out: Idle, right_down: Run, left_down: Run, left_up: Run, right_up: Run}
@@ -159,9 +168,6 @@ class StateMachine:
 
     def draw(self):
         self.cur_state.draw(self.boy)
-
-
-
 
 
 class Boy:
